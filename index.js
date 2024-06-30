7e4edcfa5fbdbdc893afe393da27faccd3a22437
rburd84 <developer.rburden@gmail.com>
2024-06-29 23:04:31 -0400

    Beginning build of player factory and functionality

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const Gameboard = (function () {
-  const board = new Array(9);
+  let board = new Array(9);
   const printBoard = () => board;
   const placeMarker = (index, mark) => {
     board[index] = mark;
   };
+  const resetBoard = () => {
+    board = new Array(9);
+    return board;
+  };
+  const win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  return { printBoard, placeMarker };
+  return { printBoard, placeMarker, resetBoard };
 })();
+
+function Player(playerName, playerMark) {
+  const name = playerName.charAt(0).toUpperCase() + playerName.slice(1);
+  const mark = playerMark;
+  const { placeMarker } = Gameboard;
+  return { name, mark, placeMarker };
+}
 
 const game = {};
 
-function Player(name) {
-  this.name = name;
-  this.score = 0;
-}
+// function Player(name) {
+//   this.name = name;
+//   this.score = 0;
+// }
 
-Gameboard.placeMarker(4, "X");
-Gameboard.placeMarker(1, "O");
-Gameboard.placeMarker(3, "X");
-Gameboard.placeMarker(5, "O");
+const player1 = Player("Josh", "X");
+const player2 = Player("Paul", "O");
+
+// Gameboard.placeMarker(4, "X");
+// Gameboard.placeMarker(1, "O");
+// Gameboard.placeMarker(3, "X");
+// Gameboard.placeMarker(5, "O");
+console.log(player1);
+player1.placeMarker(4, player1.mark);
+player2.placeMarker(3, player2.mark);
+player1.placeMarker(1, player1.mark);
+player2.placeMarker(7, player2.mark);
+// console.log(Gameboard.printBoard());
+// Gameboard.resetBoard();
 console.log(Gameboard.printBoard());
