53a06b9499ce5ab3798cec3cecf0dcf1bcf0ad32
rburd84 <developer.rburden@gmail.com>
2024-06-29 21:01:19 -0400

    Beginning gameboard functionality

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Tapped in...");
+const Gameboard = (function () {
+  const board = new Array(9);
+  const printBoard = () => board;
+  const placeMarker = (index, mark) => {
+    board[index] = mark;
+  };
+
+  return { printBoard, placeMarker };
+})();
+
+const game = {};
+
+function Player(name) {
+  this.name = name;
+  this.score = 0;
+}
+
+Gameboard.placeMarker(4, "X");
+Gameboard.placeMarker(1, "O");
+Gameboard.placeMarker(3, "X");
+Gameboard.placeMarker(5, "O");
+console.log(Gameboard.printBoard());
